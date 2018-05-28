1533b79496dfde7cae91ca0ea74158b8a67e57c1
Alexandru Olaru <alxolr@gmail.com>
2018-05-28 18:32:22 +0300

    Worked on streaming interface

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports = function init() {
-  console.log('Used only as command line tool');
-};
+'use strict';
+
+const { Readable } = require('stream');
+const fs = require('fs');
+const path = require('path');
+
+const ignoredFolders = [
+  'node_modules',
+  '.git',
+  '.cfignore',
+  'bower_components',
+];
+
+const readable = new Readable({
+  read() { },
+});
+const dir = process.argv[2] || '.';
+
+fs.readdir(dir, handleDirectories(dir));
+
+function handleDirectories(dirPath) {
+  return (err, directories) => {
+    directories.forEach(handleDescriptor(dirPath));
+  };
+}
+
+
+function handleDescriptor(dirPath) {
+  return (descriptor) => {
+    const filePath = path.join(dirPath, descriptor);
+    const file = fs.lstatSync(filePath);
+
+    if (file.isDirectory()) {
+      if (!ignoredFolders.includes(descriptor)) {
+        fs.readdir(filePath, handleDirectories(filePath));
+      }
+    } else {
+      if (/\.js$/.test(filePath)) {
+        readable.push(filePath);
+      }
+    }
+  };
+}
+
+readable
+  .pipe(process.stdout);
+

7bba41ce0a0bf629592373c68967ae751610efe6
Alexandru Olaru <alxolr@gmail.com>
2018-05-30 18:38:50 +0300

    Added the nyc output to ignore

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const DirectoryScanner = require('./lib/directory-scanner');
+
+const scanner = new DirectoryScanner({
+  ignore: [
+    'node_modules',
+    '.git'
+  ],
+  extensions: ['.js']
+});
+
+scanner.scan(process.argv[2])
+  .pipe(process.stdout);

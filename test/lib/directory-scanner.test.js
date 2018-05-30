ca28de6a43c4f8ac48c29a76fee50dbdc14898e4
Alexandru Olaru <alxolr@gmail.com>
2018-05-30 16:11:41 +0300

    Added the scan method

--- test/lib/directory-scanner.test.js
+++ test/lib/directory-scanner.test.js
@@ -2,13 +2,26 @@
 
 const DirectoryScanner = require('../../lib/directory-scanner');
 const assert = require('assert');
-const { Readable } = require('stream');
+const { Writable } = require('stream');
 const { EventEmitter } = require('events');
 
 describe('DirectoryScanner', () => {
-  it('should exist the DirectoryScanner instance of Readable', () => {
+  it('should exist the DirectoryScanner instance of EventEmitter', () => {
     const directory = new DirectoryScanner();
 
     assert.equal(directory instanceof EventEmitter, true);
+  });
+
+  it('should have the scan method that is a readable stream', (next) => {
+    const directory = new DirectoryScanner();
+    const writable = new Writable({ write() { } });
+    directory
+      .scan()
+      .on('end', () => next())
+      .pipe(writable);
+  });
+
+  it('should scan all the folders from a specified directory and push the paths', (next) => {
+    const scanner = new DirectoryScanner();
   });
 });
